011cccb5b1d0986b18d457a8873f6b4a96ce2095
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-09-10 14:21:41 +0800

    Added event listener

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,6 +23,7 @@ const cards = [
 const cardsInPlay = [];
 
 function checkForMatch() {
+
 	if(cardsInPlay.length === 2) {
 		if(cardsInPlay[0] === cardsInPlay[1]) {
 			console.log("You've found a match!");
@@ -33,14 +34,26 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(1);
+function createBoard() {
+	for(var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement)[0];
+	}
+}
+
+createBoard();
